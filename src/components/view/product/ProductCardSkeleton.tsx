553237e37c95feb323eacbc0ecf3f5d40d155a51
MgHtetMyatAung <htetmyataung16000@gmail.com>
2025-02-09 01:28:18 +0630

    changes design card height

--- src/components/view/product/ProductCardSkeleton.tsx
+++ src/components/view/product/ProductCardSkeleton.tsx
@@ -2,5 +2,5 @@ import { Skeleton } from "@/components/ui/skeleton";
 import React from "react";
 
 export default function ProductCardSkeleton() {
-  return <Skeleton className=" w-full h-[300px]"></Skeleton>;
+  return <Skeleton className=" w-full h-[200px] sm:h-[300px]"></Skeleton>;
 }
